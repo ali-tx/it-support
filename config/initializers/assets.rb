af747327eb5e0fa421677e3d910721e39bd08a75
ali-tx <ali@techcreatix.com>
2020-09-22 20:12:06 +0500

    Scsss

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,7 +7,7 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
-Rails.application.config.assets.precompile += %w( application.scss article.scss pages.scss users.scss welcome.scss *.css  )
+Rails.application.config.assets.precompile += %w( applications.scss article.scss pages.scss users.scss welcome.scss *.css  )
 
 
 # Precompile additional assets.
